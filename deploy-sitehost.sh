9a6a8b4e8e740d08eb5af6e5a9e37bc0cd01d9e9
Blair George <stepped@gmail.com>
2026-06-03 18:17:12 +1200

    Use sudo for SiteHost Docker restart

--- deploy-sitehost.sh
+++ deploy-sitehost.sh
@@ -5,5 +5,5 @@ ssh sitehost '
   cd /opt/docker/services/shiny-server/data/wkt.insightful.kiwi &&
   git pull --ff-only &&
   cd /opt/docker/services/shiny-server &&
-  docker compose restart shiny
+  sudo docker compose restart shiny
 '
